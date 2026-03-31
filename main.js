eb07a56c55717c50aa86f4c04b71eba0febaca9d
Henry Gasparotto <126111750+henrypedroso@users.noreply.github.com>
2026-03-31 11:08:31 -0300

    ssssssssssssssss

--- main.js
+++ main.js
@@ -16,3 +16,6 @@ for(let i = 0; i < botoes.length; i++){
 }
 
 const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2026-10-05");
+
+contadores[0].textContent = tempoObjetivo1;
