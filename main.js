828364fc33007e5c4a598ee89519ef69fffe248c
Henry Gasparotto <126111750+henrypedroso@users.noreply.github.com>
2026-04-15 07:16:29 -0300

    ssssssssssssssssss

--- main.js
+++ main.js
@@ -25,10 +25,15 @@ const tempos = [tempoObjetivo1, tempoObjetivo2,tempoObjetivo3, tempoObjetivo4];
 
 
 
-
+function atualizaCronometro(){
 for(let i = 0; i < contadores.length; i++){
     contadores[i].textContent = calculaTempo(tempos[i]);
 }
+}
+
+atualizaCronometro();
+setInterval(atualizaCronometro, 1000);
+
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
