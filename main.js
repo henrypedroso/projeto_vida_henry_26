ac998a233dac050f391a3216434ebac1e10fa12e
Henry Gasparotto <126111750+henrypedroso@users.noreply.github.com>
2026-04-15 07:20:13 -0300

    sssssssssssssssss

--- main.js
+++ main.js
@@ -25,16 +25,6 @@ const tempos = [tempoObjetivo1, tempoObjetivo2,tempoObjetivo3, tempoObjetivo4];
 
 
 
-function atualizaCronometro(){
-for(let i = 0; i < contadores.length; i++){
-    contadores[i].textContent = calculaTempo(tempos[i]);
-}
-}
-
-atualizaCronometro();
-setInterval(atualizaCronometro, 1000);
-
-
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
@@ -50,3 +40,16 @@ function calculaTempo(tempoObjetivo){
 
     return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
 }
+
+function atualizaCronometro(){
+for(let i = 0; i < contadores.length; i++){
+    contadores[i].textContent = calculaTempo(tempos[i]);
+}
+}
+
+function começaCronometro{
+atualizaCronometro();
+setInterval(atualizaCronometro, 1000);
+}
+
+começaCronometro();
