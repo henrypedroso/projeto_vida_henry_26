ce264bc75c28598030635e8d50eeac6bd86c7cf7
Henry Gasparotto <126111750+henrypedroso@users.noreply.github.com>
2026-05-05 12:58:39 -0300

    ssssssssssssssssssssss

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ function calculaTempo(tempoObjetivo){
     if(tempoFinal > 0 ){
     return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
     } else{
-        return "Prazo Finalizado";
+        return [0,0,0,0];
     }
 
 }
