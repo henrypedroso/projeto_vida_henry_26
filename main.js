cd13fc4474b7215bb7ec6899cf83429bef8f3622
Henry Gasparotto <126111750+henrypedroso@users.noreply.github.com>
2026-05-13 07:38:43 -0300

    sssssssssssssssssss

--- main.js
+++ main.js
@@ -49,10 +49,10 @@ function atualizaCronometro(){
     
    
     for(let i = 0; i < contadores.length; i++){
-        document.getElementById('dias0' + i).textContent = calculaTempo(tempos[i])[0];
-        document.getElementById('horas0' + i).textContent = calculaTempo(tempos[i])[1];
-        document.getElementById('min0' + i).textContent = calculaTempo(tempos[i])[2];
-        document.getElementById('seg0' + i).textContent = calculaTempo(tempos[i])[3];
+        document.getElementById('dias' + i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById('horas' + i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById('min' + i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById('seg' + i).textContent = calculaTempo(tempos[i])[3];
 
     //contadores[i].textContent = calculaTempo(tempos[i]);
 }
